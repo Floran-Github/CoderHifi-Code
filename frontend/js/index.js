04489dffed15e4263622270891e9c3af95479e99
bhagwanZaki <zaki.tarveen@gmail.com>
2021-02-23 23:24:03 +0530

    Added Mainpage with boys and girls theme

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -7,3 +7,17 @@ function showComment(button_id) {
     }
 }
 
+function toggleTheme() { 
+            // Obtains an array of all <link> 
+            // elements. 
+            // Select your element using indexing. 
+            var theme = document.getElementsByTagName('link')[4]; 
+  
+            // Change the value of href attribute  
+            // to change the css sheet. 
+            if (theme.getAttribute('href') == 'css/boy-theme.css') { 
+                theme.setAttribute('href', 'css/girls-theme.css'); 
+            } else { 
+                theme.setAttribute('href', 'css/boy-theme.css'); 
+            } 
+        } 
